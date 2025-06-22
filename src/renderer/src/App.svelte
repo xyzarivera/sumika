76f622fbe15bbc0878582031ac8374739eb46b15
Xyza Rivera <xyzarivera@gmail.com>
2025-06-22 23:26:40 +0900

    use alias path in svelte renderer

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,5 +1,5 @@
-<script>
-  import Clock from "../src/components/Clock.svelte";
+<script lang="ts">
+  import Clock from "@/components/Clock.svelte";
 </script>
 
 <main class="flex h-full w-full items-center justify-center bg-white">
