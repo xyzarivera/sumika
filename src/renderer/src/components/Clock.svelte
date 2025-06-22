08e9ed7bbde3e25a9cf05bfac5fe250a72b08d28
Xyza Rivera <xyzarivera@gmail.com>
2025-06-22 16:27:55 +0900

    Display current date and day

--- src/renderer/src/components/Clock.svelte
+++ src/renderer/src/components/Clock.svelte
@@ -3,14 +3,20 @@
   import { onMount } from "svelte";
 
   let current_time = dayjs().format("hh:mm A");
+  let current_date = dayjs().format("MMM DD YYYY, ddd");
 
   onMount((): (() => void) => {
     const interval = setInterval(() => {
       current_time = dayjs().format("hh:mm A");
+      current_date = dayjs().format("MMM DD YYYY, ddd");
     }, 1000);
 
     return () => clearInterval(interval);
   });
 </script>
 
-<p>{current_time}</p>
+<section>
+  <h1 hidden>datetime today</h1>
+  <p>{current_time}</p>
+  <p>{current_date}</p>
+</section>
