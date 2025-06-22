98a1a543d1073030cceb7082ee499feaa18d50bf
Xyza Rivera <xyzarivera@gmail.com>
2025-06-22 15:30:42 +0900

    Show Clock component in the display

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,1 +1,7 @@
-<div class="flex h-full w-full items-center justify-center bg-white text-red-500">Hello World</div>
+<script>
+  import Home from "../src/pages/home.svelte";
+</script>
+
+<main class="flex h-full w-full items-center justify-center bg-white">
+  <Home />
+</main>
---
+++ src/renderer/src/pages/home.svelte
@@ -0,0 +1,5 @@
+<script>
+  import Clock from "../components/Clock.svelte";
+</script>
+
+<Clock />
